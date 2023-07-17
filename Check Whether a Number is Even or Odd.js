270bc0d615882abff10e9215621d1ab8428eb532
Harshil Chavada <harshilchavada04@gmail.com>
2023-07-17 23:31:03 +0530

    File upload

---
+++ Check Whether a Number is Even or Odd.js
@@ -0,0 +1,11 @@
+// function fnc(num) {
+//   return !(num & 1);
+// }
+// if (fnc(12)) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+num = 29;
+console.log(num % 2 === 0 ? "Even" : "Odd");
